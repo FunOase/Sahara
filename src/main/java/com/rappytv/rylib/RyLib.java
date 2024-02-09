1d89db1f01818d992ed1e9b41ded3c2dbf39a1ae
RappyTV <rappytv@rappytv.com>
2024-02-09 15:36:03 +0100

    Add main file

--- src/main/java/com/rappytv/rylib/RyLib.java
+++ src/main/java/com/rappytv/rylib/RyLib.java
@@ -1,17 +1,32 @@
 package com.rappytv.rylib;
 
+import com.rappytv.rylib.command.RyLibCommand;
+import com.rappytv.rylib.util.I18n;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@SuppressWarnings("unused")
 public final class RyLib extends JavaPlugin {
+
+    private I18n i18n;
+    private static RyLib instance;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig();
+        i18n = new I18n(this);
+        instance = this;
 
+        new RyLibCommand("rylib", this);
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public void onDisable() {}
+
+    public I18n i18n() {
+        return i18n;
+    }
+
+    public static RyLib get() {
+        return instance;
     }
 }

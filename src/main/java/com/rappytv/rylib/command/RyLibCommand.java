2ef7bb177125947057de92d648ecd312abb65096
RappyTV <rappytv@rappytv.com>
2024-02-09 15:35:51 +0100

    Implement reload command

---
+++ src/main/java/com/rappytv/rylib/command/RyLibCommand.java
@@ -0,0 +1,29 @@
+package com.rappytv.rylib.command;
+
+import com.rappytv.rylib.RyLib;
+import com.rappytv.rylib.util.Command;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+
+public class RyLibCommand extends Command<RyLib> {
+
+    public RyLibCommand(String name, RyLib plugin) {
+        super(name, plugin);
+    }
+
+    @Override
+    public void execute(CommandSender sender, String prefix, String[] args) {
+        if(!sender.hasPermission("rylib.reload")) {
+            sender.sendMessage(plugin.i18n().translate("msg.noPermission"));
+            return;
+        }
+        plugin.reloadConfig();
+        sender.sendMessage(plugin.i18n().translate("private.reload"));
+    }
+
+    @Override
+    public List<String> complete(CommandSender sender, String prefix, String[] args) {
+        return null;
+    }
+}

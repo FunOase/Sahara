f796de7e12d5ead8592162b3570f178fb03c7e4a
RappyTV <rappytv@rappytv.com>
2024-02-10 02:49:07 +0100

    Register UpdateChecker for rylib and PlayerListener

--- src/main/java/com/rappytv/rylib/RyLib.java
+++ src/main/java/com/rappytv/rylib/RyLib.java
@@ -1,7 +1,11 @@
 package com.rappytv.rylib;
 
 import com.rappytv.rylib.command.RyLibCommand;
+import com.rappytv.rylib.listeners.PlayerListener;
 import com.rappytv.rylib.util.I18n;
+import com.rappytv.rylib.util.UpdateChecker;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("unused")
@@ -14,9 +18,18 @@ public final class RyLib extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
         i18n = new I18n(this);
+        new UpdateChecker<>(this).setArtifactFormat(
+                        "ci.rappytv.com",
+                        "RY-Lib",
+                        "com.rappytv",
+                        "Minecraft Plugins"
+        );
         instance = this;
 
         new RyLibCommand("rylib", this);
+
+        PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(new PlayerListener(this), this);
     }
 
     @Override

5f991775ee7b1615d751cb690eaacec0a01bc335
RappyTV <rappytv@rappytv.com>
2024-02-11 03:44:46 +0100

    Renamed color util class, add function to just translate without player

--- src/main/java/com/rappytv/rylib/util/Colors.java
+++ src/main/java/com/rappytv/rylib/util/Colors.java
@@ -1,11 +1,12 @@
 package com.rappytv.rylib.util;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Util {
+public class Colors {
 
     public static final Pattern hex = Pattern.compile("#[a-fA-F0-9]{6}");
     private static final Pattern color = Pattern.compile("(?i)&([0-9A-FR])");
@@ -15,7 +16,17 @@ public class Util {
     private static final Pattern underline = Pattern.compile("(?i)&([N])");
     private static final Pattern italic = Pattern.compile("(?i)&([O])");
 
-    public static String translateColorCodes(Player player, String message, String basePermission) {
+    public static String translateCodes(String message) {
+        Matcher match = hex.matcher(message);
+        while(match.find()) {
+            String color = message.substring(match.start(), match.end());
+            message = message.replace(color, net.md_5.bungee.api.ChatColor.of(color) + "");
+            match = hex.matcher(message);
+        }
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+    public static String translatePlayerCodes(Player player, String message, String basePermission) {
         if(player.hasPermission(basePermission + ".hex")) {
             Matcher match = hex.matcher(message);
             while(match.find()) {

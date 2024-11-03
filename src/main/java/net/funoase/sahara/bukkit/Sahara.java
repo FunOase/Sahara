09b503ea818f09b3609537621a9394d21c7efa0c
RappyTV <rappytv@rappytv.com>
2024-11-03 03:03:07 +0100

    Load translations

--- src/main/java/net/funoase/sahara/bukkit/Sahara.java
+++ src/main/java/net/funoase/sahara/bukkit/Sahara.java
@@ -1,38 +1,27 @@
 package net.funoase.sahara.bukkit;
 
 import net.funoase.sahara.bukkit.command.SaharaCommand;
-import net.funoase.sahara.common.database.Database;
 import net.funoase.sahara.common.i18n.I18nManager;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SuppressWarnings("unused")
 public final class Sahara extends JavaPlugin {
 
+    private static final Logger log = LoggerFactory.getLogger(Sahara.class);
     private static Sahara instance;
     private I18nManager i18nManager;
-    private Database database;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        loadDatabase();
-        i18nManager = new I18nManager(getLogger());
-        database = new Database(getLogger());
         instance = this;
+        i18nManager = new I18nManager(getLogger());
+        i18nManager.saveTranslations(this);
 
+        // Register commands
         new SaharaCommand("sahara", this).register();
-    }
-
-    private void loadDatabase() {
-        FileConfiguration config = getConfig();
-        this.database.connect(
-                config.getString("database.host"),
-                config.getInt("database.port"),
-                config.getString("database.database"),
-                config.getString("database.username"),
-                config.getString("database.password")
-        );
     }
 
     public I18nManager getI18nManager() {
